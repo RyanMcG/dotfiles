b6285b2020bec052a99849791bcace521be95595
Ryan McGowan <ryan@ryanmcg.com>
2011-09-22 01:05:29 -0400

    Modified .config/autostart.sh

--- .config/autostart.sh
+++ .config/autostart.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 #---- Autostart regardless of WM ----#
 
-nitrogen --restore &    #Restores wallpaper
+#nitrogen --restore &    #Restores wallpaper
 xmodmap ~/.Xmodmap &
 xset m 5 10 &
 xset b off &
 unclutter -idle 2 &
 urxvtd &                #Runs rxvt-unicode deamon
-dropbox start &
+#dropbox start &
 start-pulseaudio-x11 &
 gnome-keyring-daemon &
-padevchooser &
+#padevchooser &
 pnmixer &
