2e40fc4f25559dc4b08dd78c851812adc10a0701
Ryan McGowan <ryan@ryanmcg.com>
2011-09-30 10:32:25 -0400

    Added dropbox back to autostart and the xfce4-power-manager.

--- .config/autostart.sh
+++ .config/autostart.sh
@@ -3,11 +3,12 @@
 
 unclutter -idle 2 &
 urxvtd &                #Runs rxvt-unicode deamon
-#dropbox start &
+dropbox start &
 start-pulseaudio-x11 &
 gnome-keyring-daemon &
 #padevchooser &
 pnmixer &
+xfce4-power-manager &
 nitrogen --restore &    #Restores wallpaper
 xmodmap ~/.Xmodmap &
 xset m 5 10 &
