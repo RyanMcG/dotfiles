6e845ca19a60253c70f8b8d5854f744fa03b99a8
Ryan McGowan <ryan@ryanmcg.com>
2012-05-02 18:28:11 -0400

    Added injection for clj-stacktrace.

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,11 +1,17 @@
-{:user {:plugins [[clj-stacktrace "0.2.4"]
-                  [lein-tarsier "0.9.2-SNAPSHOT"]
+{:user {:plugins [[lein-tarsier "0.9.2-SNAPSHOT"]
                   [lein-swank "1.4.4"]
+                  [lein-difftest "1.3.7"]
+                  [clj-stacktrace "0.2.4"]
                   [lein-pprint "1.1.1"]
                   [slamhound "1.2.0"]
                   [lein-cljsbuild "0.1.8"]
                   ;[lein-autodoc "0.9.0"]
                   [lein-marginalia "0.7.0"]
                   [lein-noir "1.2.0"]]
+        :injections [(let [orig (ns-resolve (doto 'clojure.stacktrace require)
+                                            'print-cause-trace)
+                           new (ns-resolve (doto 'clj-stacktrace.repl require)
+                                           'pst)]
+                       (alter-var-root orig (constantly @new)))]
         :vimclojure-opts {:repl true
                           :port 2113}}}
