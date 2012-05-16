2ac1370390613b3a20ec65785b0fca2a1b9b3aec
Ryan McGowan <ryan@ryanmcg.com>
2012-05-16 18:47:28 -0400

    Uncommet clj-stacktrace injection.

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -4,14 +4,14 @@
                   [clj-stacktrace "0.2.4"]
                   [lein-pprint "1.1.1"]
                   [slamhound "1.2.0"]
-                  [lein-cljsbuild "0.1.8"]
+                  [lein-cljsbuild "0.1.9"]
                   ;[lein-autodoc "0.9.0"]
                   [lein-marginalia "0.7.0"]
                   [lein-noir "1.2.0"]]
-        ;:injections [(let [orig (ns-resolve (doto 'clojure.stacktrace require)
-                                            ;'print-cause-trace)
-                           ;new (ns-resolve (doto 'clj-stacktrace.repl require)
-                                           ;'pst)]
-                       ;(alter-var-root orig (constantly @new)))]
+        :injections [(let [orig (ns-resolve (doto 'clojure.stacktrace require)
+                                            'print-cause-trace)
+                           new (ns-resolve (doto 'clj-stacktrace.repl require)
+                                           'pst)]
+                       (alter-var-root orig (constantly @new)))]
         :vimclojure-opts {:repl true
                           :port 2113}}}
