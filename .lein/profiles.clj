39f3754d5af2f0b98c788dfdbca81a2302f45dd9
Ryan McGowan <ryan@ryanmcg.com>
2012-08-11 11:56:54 -0700

    Upgrades some lein plugins.

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,7 +1,7 @@
 {:user {:plugins [[lein-tarsier "0.9.1"]
                   [lein-swank "1.4.4"]
                   [lein-difftest "1.3.7"]
-                  [lein-clojars "0.8.0"]
+                  [lein-clojars "0.9.0"]
                   ;[jark/jark-server "0.4.0"]
                   [clj-stacktrace "0.2.4"]
                   [lein-pprint "1.1.1"]
@@ -9,13 +9,11 @@
                   [lein-cljsbuild "0.1.9"]
                   [lein-deps-tree "0.1.1"]
                   ;[lein-autodoc "0.9.0"]
-                  [lein-marginalia "0.7.0"]
-                  [lein-noir "1.2.0"]]
+                  [lein-marginalia "0.7.1"]]
         :repl-options {:timeout 60000}
-        :injections [(let [orig (ns-resolve (doto 'clojure.stacktrace require)
-                                            'print-cause-trace)
-                           new (ns-resolve (doto 'clj-stacktrace.repl require)
-                                           'pst)]
-                       (alter-var-root orig (constantly @new)))]
-        :vimclojure-opts {:repl true
-                          :port 2113}}}
+        ;:injections [(let [orig (ns-resolve (doto 'clojure.stacktrace require)
+                                            ;'print-cause-trace)
+                           ;new (ns-resolve (doto 'clj-stacktrace.repl require)
+                                           ;'pst)]
+                       ;(alter-var-root orig (constantly @new)))]
+        :vimclojure-opts {:repl true}}}
