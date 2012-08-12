5eebdb7e862d9dda44161161c4f96f9efcdf8be4
Ryan McGowan <ryan@ryanmcg.com>
2012-08-11 18:17:04 -0700

    More tweaks to .lein/profiles.clj.

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,9 +1,9 @@
-{:user {:plugins [[lein-tarsier "0.9.1"]
+{:user {:dependencies [[clj-stacktrace "0.2.4"]]
+        :plugins [[lein-tarsier "0.9.1"]
                   [lein-swank "1.4.4"]
                   [lein-difftest "1.3.7"]
                   [lein-clojars "0.9.0"]
                   ;[jark/jark-server "0.4.0"]
-                  [clj-stacktrace "0.2.4"]
                   [lein-pprint "1.1.1"]
                   [slamhound "1.2.0"]
                   [lein-cljsbuild "0.1.9"]
@@ -11,9 +11,9 @@
                   ;[lein-autodoc "0.9.0"]
                   [lein-marginalia "0.7.1"]]
         :repl-options {:timeout 60000}
-        ;:injections [(let [orig (ns-resolve (doto 'clojure.stacktrace require)
-                                            ;'print-cause-trace)
-                           ;new (ns-resolve (doto 'clj-stacktrace.repl require)
-                                           ;'pst)]
-                       ;(alter-var-root orig (constantly @new)))]
+        :injections [(let [orig (ns-resolve (doto 'clojure.stacktrace require)
+                                            'print-cause-trace)
+                           new (ns-resolve (doto 'clj-stacktrace.repl require)
+                                           'pst)]
+                       (alter-var-root orig (constantly @new)))]
         :vimclojure-opts {:repl true}}}
