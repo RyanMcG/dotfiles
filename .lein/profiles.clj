3f5c90543e820dd2164ff5bb4e32d466aaa1c16d
Ryan McGowan <ryan@ryanmcg.com>
2012-10-31 19:23:59 -0400

    Increment clj-stacktrace version.

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,4 +1,4 @@
-{:user {:dependencies [[clj-stacktrace "0.2.4"]
+{:user {:dependencies [[clj-stacktrace "0.2.5"]
                        [spyscope "0.1.0"]]
         :plugins [[lein-tarsier "0.9.4-SNAPSHOT"]
                   [lein-swank "1.4.4"]
