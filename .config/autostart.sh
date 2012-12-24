1731ba54a106c31d48ae304ec1f6288560447ec9
Ryan McGowan <ryan@ryanmcg.com>
2012-12-24 18:58:30 -0500

    Remove non-existant commands from autostart.

--- .config/autostart.sh
+++ .config/autostart.sh
@@ -2,11 +2,9 @@
 #---- Autostart regardless of WM ----#
 
 start-pulseaudio-x11 &
-gnome-keyring-daemon &
 unclutter -idle 2 &
 urxvtd &                #Runs rxvt-unicode deamon
 dropbox start &
-#padevchooser &
 pnmixer &
 xfce4-power-manager &
 xmodmap ~/.Xmodmap &
