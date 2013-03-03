1c0cc06806d798a33e477124c02aafe3b196cdff
Ryan McGowan <ryan@ryanmcg.com>
2013-03-02 20:07:25 -0500

    Remove nrepl transcript.

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,6 +1,5 @@
 {:user {:dependencies [[clj-stacktrace "0.2.5"]
                        [spyscope "0.1.0"]
-                       [nrepl-transcript "0.1.0"]
                        [limit-break "0.1.0-SNAPSHOT"]]
         :plugins [[lein-difftest "1.3.7"]
                   [lein-drip "0.1.1-SNAPSHOT"]
@@ -11,9 +10,7 @@
                   [lein-cljsbuild "0.1.9"]
                   [lein-deps-tree "0.1.2"]
                   [lein-marginalia "0.7.1"]]
-        :repl-options {:timeout 120000
-                       :nrepl-middleware
-                       [nrepl-transcript.file/wrap-file-transcript]}
+        :repl-options {:timeout 120000}
         :injections [(let [orig (ns-resolve (doto 'clojure.stacktrace require)
                                             'print-cause-trace)
                            new (ns-resolve (doto 'clj-stacktrace.repl require)
