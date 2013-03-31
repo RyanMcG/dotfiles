123b132abaaaacaff802ae5fd74bd3f7d93b26b7
Ryan McGowan <ryan@ryanmcg.com>
2013-03-31 18:20:10 -0400

    Modify compile_custom_stylesheet script for multi platform.
    Doesn't really work though :(.

--- misc/User StyleSheets/compile_custom_stylesheet.sh
+++ misc/User StyleSheets/compile_custom_stylesheet.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
-DEST_DIR=~/Library/Application\ Support/Google/Chrome/Default/User\ StyleSheets
-
+if [[ $(uname) != "Darwin" ]]; then
+	DEST_DIR=~/Library/Application\ Support/Google/Chrome/Default/User\ StyleSheets
+else
+	DEST_DIR=~/.config/google-chrome/Default/User\ StyleSheets
+fi
 lessc Custom.less $DEST_DIR/Custom.css
