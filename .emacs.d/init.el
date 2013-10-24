691a5fb4308635c24f35bb55a99973abbc0ad601
Ryan McGowan <ryan@ryanmcg.com>
2013-10-23 23:30:15 -0700

    Fix indentation in init.el.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -6,29 +6,33 @@
 ;; Setup marmalade and melpa
 (require 'package)
 (add-to-list 'package-archives
-	     '("marmalade" .  "http://marmalade-repo.org/packages/"))
+             '("marmalade" .  "http://marmalade-repo.org/packages/"))
 (add-to-list 'package-archives
-	     '("melpa" . "http://melpa.milkbox.net/packages/"))
+             '("melpa" . "http://melpa.milkbox.net/packages/"))
 
 (package-initialize)
 
 ;; Define packages for installation
 (setq my-packages
       '(auto-complete
-	clojure-mode
-	nrepl
-	; nrepl-tracing
-	ac-nrepl
-	rainbow-delimiters
-	color-theme-solarized
-	nlinum
-	smartparens
-	evil))
+        clojure-mode
+        nrepl
+        ;; nrepl-tracing
+        ac-nrepl
+        rainbow-delimiters
+        color-theme-solarized
+        nlinum
+        smartparens
+        evil))
+
+;; Fetch the list of packages available
+(when (not package-archive-contents)
+  (package-refresh-contents))
 
 ;; Install mypackages if necessary
 (mapc (lambda (pkg)
-	(unless (package-installed-p pkg)
-	  (package-install pkg)))
+        (unless (package-installed-p pkg)
+          (package-install pkg)))
       my-packages)
 
 ;; Turn on evil
@@ -50,7 +54,8 @@
 (add-hook 'clojure-mode-hook 'rainbow-delimiters-mode)
 (add-hook 'nrepl-mode-hook 'ac-nrepl-setup)
 (add-hook 'nrepl-interaction-mode-hook 'ac-nrepl-setup)
-;; (require 'smartparens-config)
+
+(require 'smartparens-config)
 (add-hook 'nrepl-repl-mode-hook 'smartparens-strict-mode)
 (eval-after-load "auto-complete"
   '(add-to-list 'ac-modes 'nrepl-mode))
