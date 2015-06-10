5644f507144c20ea1107d965234ecc182a350f0c
Ryan McGowan <mcgowanr@amazon.com>
2015-06-10 10:05:22 -0700

    Add screenlayout scripts.

---
+++ .screenlayout/only.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+xrandr --output DP1 --off --output eDP1 --mode 1600x900 --pos 0x0 --dpi 131
+refresh-hud
---
+++ .screenlayout/projector.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output HDMI2 --off --output HDMI1 --off --output DP1 --mode 1280x1024 --pos 0x0 --rotate normal --output eDP1 --mode 1600x900 --pos 0x0 --rotate normal --output DP2 --off
