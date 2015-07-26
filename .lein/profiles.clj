04837bf594eeb0c39cc73b151c83e4a95c94ad5c
Ryan McGowan <mcgowanr@amazon.com>
2015-07-26 12:32:33 -0700

    Use lein shorthand and allembic in profiles.clj.

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,11 +1,13 @@
 {:user {:dependencies [[clj-stacktrace "0.2.8"]
                        [spyscope "0.1.5"]
+                       [alembic "0.3.2"]
                        [debugger "0.1.7"]
                        [bolth "0.1.0"]
                        [com.aphyr/prism "0.1.2"]
                        [slamhound "1.5.5"]]
         :plugins [[lein-difftest "2.0.0"]
                   [lein-exec "0.3.4"]
+                  [com.palletops/lein-shorthand "0.4.0"]
                   [lein-instant-cheatsheet "2.1.3"]
                   [lein-ancient "0.5.5"]
                   [com.aphyr/prism "0.1.2"]
@@ -17,12 +19,12 @@
                   [lein-pprint "1.1.2"]
                   [lein-ring "0.8.13"]
                   [lein-cljsbuild "1.0.6"]
-                  [lein-deps-tree "0.1.2"]
                   [mvxcvi/whidbey "0.4.1"]
                   [lein-typed "0.3.5"]
                   [lein-marginalia "0.8.0"]]
         ; printing options are customizable:
         :puget-options {}
+        :shorthand {. [alembic.still/distill alembic.still/lein]}
         :repl-options {:timeout 120000}
         :injections [(require 'spyscope.core)
                      (let [orig (ns-resolve (doto 'clojure.stacktrace require)
