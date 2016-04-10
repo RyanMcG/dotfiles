526b7002c8a6bc173283e623981816ab9a5982e7
Ryan McGowan <ryan@ryanmcg.com>
2016-04-10 15:33:29 -0700

    Fix docked.sh for new setup.

--- .screenlayout/docked.sh
+++ .screenlayout/docked.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-xrandr --output DP1 --mode 1920x1080 --pos 1600x0 --output eDP1 --mode 1600x900 --pos 0x104 --dpi 102
+xrandr --output HDMI-1 --mode 1920x1080 --pos 1600x0 --output eDP-1 --mode 1600x900 --pos 0x104 --dpi 102
 refresh-hud
