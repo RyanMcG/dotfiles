8a73c923122e8a873288eb1a202910646819004d
Ryan McGowan <ryan@ryanmcg.com>
2024-05-23 17:21:05 -0600

    update lspconfig

--- .vim/lua/lsp.lua
+++ .vim/lua/lsp.lua
@@ -35,7 +35,31 @@ lspconfig.pylsp.setup {
     }
   }
 }
-lspconfig.pyright.setup {}
+local on_attach = function(client, bufnr)
+  if client.name == 'ruff' then
+    -- Disable hover in favor of Pyright
+    client.server_capabilities.hoverProvider = false
+  end
+end
+
+lspconfig.ruff.setup {
+  on_attach = on_attach,
+}
+
+lspconfig.pyright.setup {
+  settings = {
+    pyright = {
+      -- Using Ruff's import organizer
+      disableOrganizeImports = true,
+    },
+    python = {
+      analysis = {
+        -- Ignore all files for analysis to exclusively use Ruff for linting
+        ignore = { '*' },
+      },
+    },
+  },
+}
 
 
 -- Rust
