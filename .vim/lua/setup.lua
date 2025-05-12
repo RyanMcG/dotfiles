d6c6ba64665dea6334f9e6bf0c38767d978334f3
Ryan McGowan <ryan@ryanmcg.com>
2025-05-12 14:49:00 -0600

    fix conform config

--- .vim/lua/setup.lua
+++ .vim/lua/setup.lua
@@ -36,9 +36,6 @@ require("conform").setup({
     lua = { "stylua" },
     -- Conform will run multiple formatters sequentially
     python = { "ruff_fix", "ruff_format" },
-    -- Use a sub-list to run only the first available formatter
-    javascript = { { } },
-    typescript = { { } },
     go = { "gofumpt" },
   },
   format_on_save = function(bufnr)
